8448664f13335ab36e81be5eb7bfe26fbc5d85ce
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2024-05-05 19:55:12 -0300

    fix: correct required type in updateSignal for defined state types

--- src/init.ts
+++ src/init.ts
@@ -1,7 +1,7 @@
 import { SignalsManager } from "./signals_manager";
 
 let signalsManager: SignalsManager<any>;
-export type SignalsState<T = {}> = { [K in keyof T]: any };
+export type SignalsState<T = {}> = { [K in keyof T]: T[K] };
 
 export function initSignalsManager<T extends SignalsState<T>>(
   defaultState: T,
